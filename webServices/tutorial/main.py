3c9fb3a0848f7b3002bca4dde18ca2e944a11d7c
Nohely Badillo <nohely.badillo96@gmail.com>
2022-04-28 01:42:16 -0500

    lectura de queries :mag:

--- webServices/tutorial/main.py
+++ webServices/tutorial/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -15,6 +15,16 @@ def index():
 @app.route('/usuario/<last_name>/<name>')
 def user(last_name, name):
     return 'Hola, ' + last_name + name
+
+
+# Esta función sirve para mandar datos desde los queries (el link)
+# por ejemplo en este caso podría quedar así:
+# localhost:9000/datos?nombre=nohely&curso=python
+@app.route('/datos')
+def datos():
+    nombre = request.args.get('nombre', '')
+    curso = request.args.get('curso', '')
+    return 'Listado de datos: ' + nombre + ' ' + curso
 
 
 if __name__ == '__main__':
